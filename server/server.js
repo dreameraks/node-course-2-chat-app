453d593d83a9abb3a30b298394fcb96789d4d94c
Abhishek Kumar Singh <dreameraks@gmail.com>
2018-07-03 16:03:29 +0530

    Greet new User

--- server/server.js
+++ server/server.js
@@ -26,16 +26,36 @@ io.on('connection',(socket) => {
   });
 
 
+  io.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from:'Admin',
+    text:'New user joined',
+    createdAt:new Date().getTime()
+  });
+
+
+
   socket.on('createMessage',(message)=> {
 
     console.log('New message' , message);
+
 
     io.emit('newMessage', {
       from:message.from,
       text:message.text,
       createdAt:new Date().getTime()
     });
-
+/*
+    socket.broadcast.emit('newMessage', {
+      from:message.from,
+      text:message.text,
+      createdAt:new Date().getTime()
+    });
+    */
   })
 
 
