5c1decc4a744d74ceedd2eb234c3c1a0dfb6d028
Abhishek Kumar Singh <dreameraks@gmail.com>
2018-07-03 17:30:21 +0530

    UI

--- public/js/index.js
+++ public/js/index.js
@@ -6,10 +6,27 @@
 
   socket.on('newMessage',function(message) {
     console.log("New message " ,message);
+
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
   });
 
   socket.on('disconnect', function(){
     console.log("Disconnected from server");
   });
 
-;
+
+  jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+
+    socket.emit('createMessage',{
+      from:'User',
+      text:jQuery('#message').val()
+    }, function() {
+
+    });
+
+  });
